e9d4f9e49fefb88f900f10e7a4b8e6e5b953acd2
Bohdan.Krasnoschok <gmtfkl@gmail.com>
2019-06-11 11:56:28 +0300

    Add Chart For Statistic
    Add Chart For Statistic

--- src/components/result-by-subjects/ResultBySubjectsGraph.js
+++ src/components/result-by-subjects/ResultBySubjectsGraph.js
@@ -1,10 +1,53 @@
 import React, {Component} from 'react';
+import './ResultBySubjectsGraph.css';
+import CanvasJSReact from './canvasjs.react';
+var CanvasJS = CanvasJSReact.CanvasJS;
+var CanvasJSChart = CanvasJSReact.CanvasJSChart;
+
 
 export default class ResultBySubjectsGraph extends Component {
-
-    render() {
-        console.log(this.props);
-        return(<div>yrtdy</div>)
+    state = {
+        theme: "dark2",
+        title: {
+            text: "Result by topics"
+        },
+        data: [{
+            type:"column",
+            dataPoints: []
+        }]
+    }
+    componentWillMount() {
+        this.props.topicsResult.forEach((element) => {
+           this.setState((state)=>{
+               state.data[0].dataPoints.push({
+                   label:element.topic.name,
+                   y: Math.floor(element.result),
+               });
+           }) ;
+        });
     }
 
+    render() {
+        // const options = {
+        //     animationEnabled: true,
+        //     theme: "dark2",
+        //     title: {
+        //         text: "Result by topics"
+        //     },
+        //     data: [{
+        //         type: "column",
+        //         dataPoints: [
+        //             {label: "Бази даних", y: 33},
+        //             {label: "Алгоритми", y: 50},
+        //             {label: "ООП", y: 100}
+        //         ]
+        //     }]
+        // }
+        console.log(this.state);
+        return(
+            <div>
+                <CanvasJSChart options = {this.state}/>
+            </div>
+        )
+    }
 }
